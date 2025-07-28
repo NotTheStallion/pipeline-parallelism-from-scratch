c4dea2dc8f72a5011cc812dac9d8f967afefd157
The_Stallion <111058733+NotTheStallion@users.noreply.github.com>
2025-07-28 17:25:26 +0200

    Wrriting first sequenctial forward for teacher : [M] "standard-kd.py"

--- standard-kd.py
+++ standard-kd.py
@@ -5,9 +5,123 @@ import torch.distributed as dist
 from src.utils import sequential_forward, sequential_backward
 from torch.utils.data import DataLoader, TensorDataset
 from src.data import MyDataset
+import nvtx
 
 
+def _forward(microinputs, index, model_part):
+    
+    nvtx.push_range(message=f"F{index}", color="blue", domain="tspipe", 
+                            category="forward", payload=rank)
+    
+    microinput = microinputs[index]
+    
+    # time.sleep(0.3)  # Simulate some processing time
+    
+    # if not microinput.requires_grad:
+    microinput.requires_grad_(True).retain_grad()
+    
+    result = model_part(microinput)
+    
+    nvtx.pop_range(domain="tspipe")
+    return result
+    
+def _backward(microoutputs, microtargets, grad_outputs, index, loss_fn, rank, world_size):
+    nvtx.push_range(message=f"B{index}", color="red", domain="tspipe", 
+                            category="backward", payload=rank)
+    # time.sleep(0.6)  # Simulate some processing time
+    
+    if microoutputs:
+        microoutput = microoutputs[index]
+    if microtargets:
+        microtarget = microtargets[index]
+    if grad_outputs:
+        grad_output = grad_outputs[index]
+    
+    if rank == world_size - 1:
+        loss = loss_fn(microoutput, microtarget)
+        loss.backward()
+        del microoutput
+        
+        nvtx.pop_range(domain="tspipe")
+        return loss.item()
+    else:
+        microoutput.backward(grad_output)
+        del grad_output
+        del microoutput
+        
+        nvtx.pop_range(domain="tspipe")
+        return None
 
+
+def train_tspipe(nn_deep_part, nn_light_part, global_inputs, global_targets, loss_fn, T, soft_target_loss_weight, ce_loss_weight, check, epochs, data_loader_len):
+    rank = dist.get_rank()
+    world_size = dist.get_world_size()
+    loss = 0
+    
+    global_teacher_outputs = [None] * len(global_inputs)
+    global_student_outputs = [None] * len(global_inputs)
+    global_grads = [None] * len(global_inputs) 
+    
+    i_f=0
+    while True:
+        # Warmup phase
+        if i_f < world_size - 1:
+            # First forwards
+            if rank != 0:
+                # Receive inputs from the previous rank
+                nvtx.push_range(message=f"m_i{i_f} from {rank-1}", color="green", domain="tspipe", 
+                            category="comm", payload=rank)
+                # time.sleep(0.1)
+                
+                dist.recv(global_inputs[i_f], src=rank - 1)
+                
+                nvtx.pop_range(domain="tspipe")
+            
+            global_teacher_outputs[i_f] = _forward(global_inputs, i_f, nn_light_part)
+
+            if rank != world_size - 1:
+                # Send outputs to the next rank
+                nvtx.push_range(message=f"m_o{i_f} to {rank+1}", color="green", domain="tspipe", 
+                            category="comm", payload=rank)
+                # time.sleep(0.1)
+                
+                dist.send(global_teacher_outputs[i_f], dst=rank + 1)
+                
+                nvtx.pop_range(domain="tspipe")
+        else:
+            break
+        
+        i_f += 1
+    
+    return loss
+
+
+def tspipe(nn_deep_part, nn_light_part, data_loader, loss_fn, T, soft_target_loss_weight, ce_loss_weight):
+    rank = dist.get_rank()
+    world_size = dist.get_world_size()
+
+    check = True
+    dataset = MyDataset()
+    loss_fn = nn.MSELoss()
+    optimizer = torch.optim.Adam(nn_light.parameters())
+    batch_size = 12 # @param
+    epochs = 10 # @param
+    
+    # * Ensure the data is shuffled in the same way across all devices
+    generator = torch.Generator()
+    generator.manual_seed(42)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, generator=generator)
+
+    global_inputs = []
+    global_targets = []
+    for epoch in range(epochs):
+        for inputs, targets in data_loader:
+            global_inputs.extend(list(torch.chunk(inputs, world_size - 1)))
+            global_targets.extend(list(torch.chunk(targets, world_size - 1)))
+    
+    train_tspipe(nn_deep_part, nn_light_part, global_inputs, global_targets, loss_fn, T, soft_target_loss_weight, ce_loss_weight, check, epochs, len(data_loader))
+            
+    
 
 
 if __name__== "__main__":
@@ -56,8 +170,7 @@ if __name__== "__main__":
     
     dataset = MyDataset()
     loss_fn = nn.MSELoss()
-    optimizer = torch.optim.Adam(nn_deep.parameters())
-    batch_size = 16 # @param
+    batch_size = 12 # @param
     
     # * Ensure the data is shuffled in the same way across all devices
     generator = torch.Generator()
@@ -66,6 +179,8 @@ if __name__== "__main__":
     
     if rank == world_size - 1:
         print("Training DeepNN CE")
+    
+    optimizer = torch.optim.Adam(nn_deep.parameters())
 
     for epoch in range(10):
         epoch_loss = 0
@@ -103,47 +218,51 @@ if __name__== "__main__":
     
     if rank == world_size - 1:
         print("Training LightNN CE + KD")
-        
+    
+    # Saving weights of untrained LightNN
+    dist_nn_light = nn_light.state_dict()
+    
     optimizer = torch.optim.Adam(nn_light.parameters())
     
     T = 2.0  # Temperature for softening the outputs
     soft_target_loss_weight = 0.5  # Weight for the soft target loss
     ce_loss_weight = 0.5  # Weight for the cross-entropy loss   
     
-    for epoch in range(10):
-        epoch_loss = 0
-        for inputs, targets in data_loader:
-            optimizer.zero_grad()
+    # for epoch in range(10):
+    #     epoch_loss = 0
+    #     for inputs, targets in data_loader:
+    #         optimizer.zero_grad()
             
-            with torch.no_grad():
-                teacher_outputs = nn_deep(inputs)
-            student_outputs = nn_light(inputs)
+    #         with torch.no_grad():
+    #             teacher_outputs = nn_deep(inputs)
+    #         student_outputs = nn_light(inputs)
             
-            # # Pytorch version of KD
-            # soft_targets = nn.functional.softmax(teacher_outputs / T, dim=-1)
-            # soft_prob = nn.functional.log_softmax(student_outputs / T, dim=-1)
+    #         # # Pytorch version of KD
+    #         # soft_targets = nn.functional.softmax(teacher_outputs / T, dim=-1)
+    #         # soft_prob = nn.functional.log_softmax(student_outputs / T, dim=-1)
             
-            # soft_targets_loss = torch.sum(soft_targets * (soft_targets.log() - soft_prob)) / soft_prob.size()[0] * (T**2)
-            # ce_loss = loss_fn(student_outputs, targets)
-            # loss = soft_target_loss_weight * soft_targets_loss + ce_loss_weight * ce_loss
+    #         # soft_targets_loss = torch.sum(soft_targets * (soft_targets.log() - soft_prob)) / soft_prob.size()[0] * (T**2)
+    #         # ce_loss = loss_fn(student_outputs, targets)
+    #         # loss = soft_target_loss_weight * soft_targets_loss + ce_loss_weight * ce_loss
             
-            # Custom version of KD
-            y_hat = nn.functional.softmax(teacher_outputs / T, dim=-1)
-            y = nn.functional.softmax(student_outputs / T, dim=-1)
-            soft_targets_loss = torch.sum(y_hat * y.log()) * (T**2)
-            ce_loss = loss_fn(student_outputs, targets)
-            loss = - soft_targets_loss + ce_loss
+    #         # Custom version of KD
+    #         y_hat = nn.functional.softmax(teacher_outputs / T, dim=-1)
+    #         y = nn.functional.softmax(student_outputs / T, dim=-1)
+    #         soft_targets_loss = torch.sum(y_hat * y.log()) * (T**2)
+    #         ce_loss = loss_fn(student_outputs, targets)
+    #         loss = - soft_targets_loss + ce_loss
             
-            loss.backward()
-            optimizer.step()
+    #         loss.backward()
+    #         optimizer.step()
             
-            epoch_loss += loss
+    #         epoch_loss += loss
 
-        if rank == world_size - 1:
-            print(f"Epoch {epoch} loss: {epoch_loss / len(data_loader)}")
+    #     if rank == world_size - 1:
+    #         print(f"Epoch {epoch} loss: {epoch_loss / len(data_loader)}")
     
     
     
+    tspipe(nn_deep, nn_light, data_loader, loss_fn, T, soft_target_loss_weight, ce_loss_weight)
     
     
     dist.destroy_process_group()
