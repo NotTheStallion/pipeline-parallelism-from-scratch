bfe678dac00d52ce4dcced7798e6972a969e2761
The_Stallion <111058733+NotTheStallion@users.noreply.github.com>
2025-07-29 16:32:55 +0200

    Adding param for distributed dataset or not : [M] "src/data.py"

--- src/data.py
+++ src/data.py
@@ -6,13 +6,14 @@ class MyDataset(torch.utils.data.Dataset):
     """
     Dummy dataset for testing
     """
-    def __init__(self, n = 1024):
+    def __init__(self, n = 1024, distributed=True):
         self.data = torch.randn(n, 32)
         self.targets = (self.data * 1.3) - 0.65
         # Synchronize data across all ranks
-        with torch.no_grad():
-            dist.broadcast(self.data, src = 0)
-            dist.broadcast(self.targets, src = 0)
+        if distributed:
+            with torch.no_grad():
+                dist.broadcast(self.data, src = 0)
+                dist.broadcast(self.targets, src = 0)
         
     def __len__(self):
         return len(self.data)
