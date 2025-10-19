21bfdf108d871611433d683ea0a7d0873045af10
The_Stallion <111058733+NotTheStallion@users.noreply.github.com>
2025-10-19 11:38:26 +0200

    [M] "handcrafted_schedules/makespan_comparison.py" : Removing dots for easier imterpretation

--- handcrafted_schedules/makespan_comparison.py
+++ handcrafted_schedules/makespan_comparison.py
@@ -28,9 +28,9 @@ for alpha in alpha_values:
     
 
 
-plt.plot(alpha_values, tspipe_makespans, label='TSPipe', marker='o')
-plt.plot(alpha_values, zbts_p_1_makespans, label='ZBTS p-1', marker='o')
-plt.plot(alpha_values, zbts_2p_makespans, label='ZBTS 2p', marker='o')
+plt.plot(alpha_values, tspipe_makespans, label='TSPipe')
+plt.plot(alpha_values, zbts_p_1_makespans, label='ZBTS p-1')
+plt.plot(alpha_values, zbts_2p_makespans, label='ZBTS 2p')
 plt.xlabel('Alpha (Teacher Forward Time Multiplier)')
 plt.ylabel('Makespan')
 plt.title(f'Makespan vs Alpha (p={p}, m=p-1 for TSPipe and ZBTS p-1, m=2p for ZBTS 2p)')
